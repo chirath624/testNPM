a0c0a8f90c1d9854427e62e99bbf4a229b2a79c3
chirath <chirath.didulanga@axiatadigitallabs.com>
2025-01-03 17:15:43 +0530

    new

---
+++ src/Input/sanitizeInput.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import DOMPurify from "dompurify";
+
+const SanitizedInput = ({ type, placeholder, onChange, style, className, ...rest }) => {
+    const [sanitizedValue, setSanitizedValue] = useState('');
+
+    const sanitizeInput = (input) => {
+        return DOMPurify.sanitize(input, { ALLOWED_TAGS: [], ALLOWED_ATTR: [] });
+    };
+
+    const handleChange = (e) => {
+        const inputValue = e.target.value;
+        const sanitizedInput = sanitizeInput(inputValue);
+        setSanitizedValue(sanitizedInput);
+        if (onChange) {
+            onChange(sanitizedInput);
+        }
+    };
+
+    const renderInput = () => {
+        switch (type) {
+            case 'text':
+                return (
+                    <input
+                        placeholder={placeholder}
+                        value={sanitizedValue}
+                        onChange={handleChange}
+                        style={style}
+                        className={className}
+                        {...rest}
+                    />
+                );
+            case 'area':
+                return (
+                    <textarea
+                        placeholder={placeholder}
+                        value={sanitizedValue}
+                        onChange={handleChange}
+                        style={style}
+                        className={className}
+                        {...rest}
+                    />
+                );
+            default:
+                return null;
+        }
+    };
+
+    return renderInput();
+};
+
+export default SanitizedInput;
